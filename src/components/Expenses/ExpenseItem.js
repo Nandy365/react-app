c733b4db82c7fd43f6d9e058d2e358015aaa6343
Nandy365 <kumarnan217@gmail.com>
2023-10-25 09:26:05 +0530

    adding update

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,15 +2,17 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"
 //import Card from "./Card";
 const  ExpenseItem = (props) => {
+    let title = props.title
     const clickme = () => {
-        console.log("cliked!!!!")
+        title = "updated"
+        console.log(title)
     }
    
     return(
         <div className="expense-item">
            <div><ExpenseDate date={props.date}/></div>
             <div className="expense-item__description">
-            <h2>{props.model}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price">${props.price}</div>
             </div>
             <button onClick={clickme} >click</button>
